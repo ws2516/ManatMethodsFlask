3765c3c73fe51a7d5c18ffdf1bfbf63bbf0c5386
ShamPow <35538789+ws2516@users.noreply.github.com>
2021-09-09 10:56:30 -0700

    Update data_pipeline.py

--- data_pipeline.py
+++ data_pipeline.py
@@ -6,12 +6,14 @@ import pandas as pd
 def go(store_name):
 	if store_name == 'MLB':
 		final_df = update_with_df.get_from_sheet('MLB')
-	elif store_name == 'WNBA':
-		final_df = update_with_df.get_from_sheet('WNBA')
+	'''elif store_name == 'WNBA':
+		final_df = update_with_df.get_from_sheet('WNBA')'''
 	elif store_name == 'NBA':
 		final_df = update_with_df.get_from_sheet('NBA')
 	elif store_name == 'NSL':
 		final_df = update_with_df.get_from_sheet('NSL')
+	elif store_name == 'AAL':
+		final_df = update_with_df.get_from_sheet('AAL')
 	return final_df
 	
 def webify(df):
