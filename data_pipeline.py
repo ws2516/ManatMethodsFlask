32d9f8b8cf47a7dbf4f41cdb38023927efb02107
ShamPow <35538789+ws2516@users.noreply.github.com>
2022-04-22 20:07:18 -0700

    Update data_pipeline.py

--- data_pipeline.py
+++ data_pipeline.py
@@ -6,8 +6,6 @@ import pandas as pd
 def go(store_name):
 	if store_name == 'MLB':
 		final_df = update_with_df.get_from_sheet('MLB')
-	'''elif store_name == 'WNBA':
-		final_df = update_with_df.get_from_sheet('WNBA')'''
 	elif store_name == 'NBA':
 		final_df = update_with_df.get_from_sheet('NBA')
 	elif store_name == 'NSL':
